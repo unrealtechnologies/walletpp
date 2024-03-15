ef8b1b5700c070524ab41144ed19d58a96d35a22
Ariel Saldana <ariel@unreal.io>
2024-03-15 19:57:21 -0400

    Separate hd_node implementation from header.
    Changed insert_child code to prevent the raw pointer from losing access to the underlying value

--- src/hd_node.cpp
+++ src/hd_node.cpp
@@ -3,3 +3,21 @@
 //
 
 #include "hd_node.h"
+
+hd_node *hd_node::insert_child(size_t index) {
+    // Check if the child already exists and return it if so
+    auto search = children.find(index);
+    if (search != children.end()) {
+        return search->second.get(); // Return existing node
+    }
+
+    // Generate a new keypair and create a new node
+    auto keypair_to_insert = hd_derivation::private_and_public_key_pair_derivation(k_pair.private_key, index);
+    auto node_to_insert = std::make_unique<hd_node>(keypair_to_insert);
+
+    // Insert the new node into the map
+    auto [it, inserted] = children.emplace(index, std::move(node_to_insert));
+
+    // Return the raw pointer to the newly inserted node
+    return it->second.get();
+}
