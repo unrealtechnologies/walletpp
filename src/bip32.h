7fcc6c53e7137f205178dbe24f72d9f9d6450f17
Ariel Saldana <ariel@unreal.io>
2024-03-23 17:13:08 -0400

    Update BIP32 class functions, to better reflect the bip standard

--- src/bip32.h
+++ src/bip32.h
@@ -9,36 +9,32 @@
 #include "hd_trie.h"
 #include "key_pair.h"
 
-#include <utility>
 namespace walletpp {
     class bip32 {
         const key_pair master_extended_key_pair_;
         const std::unique_ptr<hd_trie> tree;
 
-
         key_pair from_base_58(const std::string_view &key);
         key_pair derive_path(const std::string_view &path);
 
     public:
-        explicit bip32(key_pair master_extended_key_pair) : master_extended_key_pair_(std::move(master_extended_key_pair)), tree([&]() -> std::unique_ptr<hd_trie> {
-                                                                return std::make_unique<hd_trie>(master_extended_key_pair_);
-                                                            }()) {}
+        explicit bip32(key_pair master_extended_key_pair)
+            : master_extended_key_pair_(std::move(master_extended_key_pair)),
+              tree([&]() -> std::unique_ptr<hd_trie> { return std::make_unique<hd_trie>(master_extended_key_pair_); }()) {}
 
-        static bip32 from_entropy(const Botan::secure_vector<uint8_t> &entropy) {
+        static bip32 from_seed(const Botan::secure_vector<uint8_t> &entropy) {
             const auto master_key_pair = walletpp::master_key_generator::generate_master_key_pair(entropy);
             return bip32(*master_key_pair);
         }
 
-        static bip32 from_entropy(const std::array<uint8_t, pbkdf2_sha512_output_byte_size> &entropy_arr) {
+        static bip32 from_seed(const std::array<uint8_t, pbkdf2_sha512_output_byte_size> &entropy_arr) {
             const auto master_key_pair = walletpp::master_key_generator::generate_master_key_pair(Botan::secure_vector<uint8_t>{entropy_arr.begin(), entropy_arr.end()});
             return bip32(*master_key_pair);
         }
 
-        [[nodiscard]] auto derive_keypair_with_path(const std::string_view path) const -> hd_node * {
-            return tree->search(path);
-        }
+        [[nodiscard]] auto derive_keypair_with_path(const std::string_view path) const -> hd_node * { return tree->search(path); }
     };
-}
+}// namespace walletpp
 
 
 #endif//BIP32_H
