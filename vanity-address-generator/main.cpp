42bd8dbe892c1f7641ca7bd325c71677bfec45ea
Ariel Saldana <ariel@unreal.io>
2024-03-29 11:28:05 -0400

    emulate a log source, for more clarity

--- vanity-address-generator/main.cpp
+++ vanity-address-generator/main.cpp
@@ -18,7 +18,7 @@ auto log(const std::string msg) -> void {
     const auto in_time_t = std::chrono::system_clock::to_time_t(now);
 
     std::stringstream ss;
-    ss << std::put_time(std::localtime(&in_time_t), "%Y/%m/%d %I:%M%p") << " " << msg;
+    ss << std::put_time(std::localtime(&in_time_t), "%Y/%m/%d %I:%M%p") << " application: " << msg;
     const std::string log_msg = ss.str();
 
     // Now log_msg contains the date, time and the message
