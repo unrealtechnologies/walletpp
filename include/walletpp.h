4dbe1111527bc204fd7715c5d6bc9047b6135565
Ariel Saldana <ariel@unreal.io>
2024-04-15 18:46:17 -0400

    Include all library headers in the walletpp.h file

--- include/walletpp.h
+++ include/walletpp.h
@@ -5,4 +5,21 @@
 #ifndef WALLETPP_H
 #define WALLETPP_H
 
+#include "bip32.h"
+#include "bip39.h"
+#include "bip44.h"
+#include "coin_type.h"
+#include "constants.h"
+#include "crypto_algorithms.h"
+#include "entropy.h"
+#include "ethereum_utils.h"
+#include "extended_key.h"
+#include "hd_derivation.h"
+#include "hd_node.h"
+#include "hd_trie.h"
+#include "master_key_generator.h"
+#include "secp256k1_context_singleton.h"
+#include "secure_vector.h"
+#include "wordlist.h"
+
 #endif //WALLETPP_H
