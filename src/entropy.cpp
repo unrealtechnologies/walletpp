7452f051816a07e6f3128ca3a04ef6691f78a1a1
Ariel Saldana <ariel@unreal.io>
2024-07-05 20:56:59 -0400

    Fix comment styling

--- src/entropy.cpp
+++ src/entropy.cpp
@@ -8,8 +8,8 @@
 namespace walletpp {
     secure_vector<uint8_t> entropy::generate_entropy(const size_t byte_size) {
         duthomhas::csprng rng;
-        secure_vector<uint8_t> random_data(byte_size);// Create a vector with the desired size first
-        rng(random_data);                             // Then pass it to the rng
+        secure_vector<uint8_t> random_data(byte_size);  // Create a vector with the desired size first
+        rng(random_data);                           // Then pass it to the rng
 
         return random_data;
     }
